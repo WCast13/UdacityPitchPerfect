00968d2d48b9d7ef7a302aef1f6118ba695d7859
William Castellano <castellkid@aol.com>
2018-05-23 08:01:39 -0400

    Audio now passed from record view controller to play sounds view controller

--- pitchPerfect/PlaySoundsViewController.swift
+++ pitchPerfect/PlaySoundsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+  
+  var recordedAudioURL: URL!
 
     override func viewDidLoad() {
         super.viewDidLoad()
